95f2cb71dc0ff23f38798d7b11441eb2e5094a53
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2023-09-21 21:43:51 +0000

    add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do https://www.tabnews.com.br/ com o objetivo de aprender programação pelo curso https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu te amo, meu bem 🌻❤️🐻</h1>
+  return <h1>Eu te amo, meu bem 🌻❤️🐻</h1>;
 }
 
 export default Home;
